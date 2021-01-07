7f875d444cc42b19a8e0d4d006a7fa6b1a2bb467
HongyangZhu <310407858@qq.com>
2021-01-07 14:51:21 +0900

    Constants ADD

---
+++ src/main/java/Constants.java
@@ -0,0 +1,43 @@
+public class Constants {
+    /**
+     * 保持不动
+     */
+    public static final int STAY = 0;
+    /**
+     * 上
+     */
+    public static final int UP = 1;
+    /**
+     * 下
+     */
+    public static final int DOWN = 2;
+    /**
+     * 左
+     */
+    public static final int LEFT = 3;
+    /**
+     * 右
+     */
+    public static final int RIGHT = 4;
+
+    /**
+     * 鬼
+     */
+    public static final int GHOST = -1;
+    /**
+     * 空地
+     */
+    public static final int SPACE = -2;
+    /**
+     * 大豆子
+     */
+    public static final int BIGPAC = -3;
+    /**
+     * 小豆子
+     */
+    public static final int SMALLPAC = -4;
+    /**
+     * 障碍物
+     */
+    public static final int[] BARRIER = {-5, -6, -7};
+}
--- src/main/java/JavaPlayer.java
+++ src/main/java/JavaPlayer.java
@@ -10,47 +10,7 @@
  */
 
 public class JavaPlayer {
-    /**
-     * 保持不动
-     */
-    public static final int STAY = 0;
-    /**
-     * 上
-     */
-    public static final int UP = 1;
-    /**
-     * 下
-     */
-    public static final int DOWN = 2;
-    /**
-     * 左
-     */
-    public static final int LEFT = 3;
-    /**
-     * 右
-     */
-    public static final int RIGHT = 4;
 
-    /**
-     * 鬼
-     */
-    public static final int GHOST = -1;
-    /**
-     * 空地
-     */
-    public static final int SPACE = -2;
-    /**
-     * 大豆子
-     */
-    public static final int BIGPAC = -3;
-    /**
-     * 小豆子
-     */
-    public static final int SMALLPAC = -4;
-    /**
-     * 障碍物
-     */
-    public static final int[] BARRIER = {-5, -6, -7};
     /**
      * 选手的初始位置
      */
@@ -84,7 +44,7 @@ public class JavaPlayer {
      */
     public int run(int[] board, int strength) throws Exception {
 
-        return UP;
+        return Constants.UP;
     }
 
 
