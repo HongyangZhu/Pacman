ca28725a5dc384427c1c5a452629bff26c6af676
HongyangZhu <310407858@qq.com>
2021-01-12 19:53:13 +0900

    删除多余的方法以及测试类

--- src/main/java/utils/CommentUtils.java
+++ src/main/java/utils/CommentUtils.java
@@ -5,9 +5,6 @@ import pojo.LocationInfo;
 
 import java.util.*;
 
-import static java.util.Map.Entry.comparingByValue;
-import static java.util.stream.Collectors.toMap;
-
 /**
  * 共通方法类
  *
@@ -19,8 +16,8 @@ public class CommentUtils {
      * 一维数组转化为二维数组
      *
      * @param array 一维数组
-     * @param line 　行
-     * @param row  列
+     * @param line  　行
+     * @param row   列
      * @return 二维数组
      */
     public static int[][] switchArray(int[] array, int line, int row) {
@@ -35,29 +32,6 @@ public class CommentUtils {
             resultArray[a][b] = num;
         }
         return resultArray;
-    }
-
-    /**
-     * 在二维数组查找指定元素 返回X，Y轴坐标
-     *
-     * @param target 指定元素
-     * @param array  二维数组
-     * @return X，Y轴坐标
-     */
-    public static LocationInfo Find(int target, int[][] array) {
-        int row = array.length;//行数
-        int col = array[0].length;//列数
-        int i = 0;
-        int j = col - 1;
-        while (i < row && j >= 0) {
-            if (array[i][j] == target)
-                return new LocationInfo(j, i);
-            else if (array[i][j] < target)
-                i++;
-            else
-                j--;
-        }
-        return new LocationInfo(0, 0);
     }
 
     /**
@@ -84,6 +58,7 @@ public class CommentUtils {
      *
      * @param array 地图
      * @return 所有玩家的坐标列表
+     * K:玩家位置，V:力量值
      */
     public static Map<LocationInfo, Integer> FindPlayer(int[][] array) {
         Map<LocationInfo, Integer> resultMap = new HashMap<>();
@@ -109,47 +84,13 @@ public class CommentUtils {
      */
     public static Map<LocationInfo, Integer> getAllDistance(LocationInfo myLocationInfo, List<LocationInfo> locationInfoList) {
         Map<LocationInfo, Integer> resultMap = new HashMap<>();
-        int sumDistance;
         if (locationInfoList != null) {
             // 遍历豆子的位置列表 计算每个豆子到自己的距离
             for (LocationInfo l : locationInfoList) {
-                sumDistance = 0;
-                sumDistance += Math.abs(l.getX() - myLocationInfo.getX());
-                sumDistance += Math.abs(l.getY() - myLocationInfo.getY());
-                resultMap.put(l, sumDistance);
+                resultMap.put(l, getDistance(l, myLocationInfo));
             }
         }
         return resultMap;
-    }
-
-    /**
-     * 寻找离自己最近的豆子
-     *
-     * @param myLocationInfo   我的当前位置
-     * @param pacLocationInfoList 豆子的位置列表
-     * @return 最近的豆子位置
-     */
-    public static LocationInfo FindNearestPac(LocationInfo myLocationInfo, List<LocationInfo> pacLocationInfoList) {
-        LocationInfo resultLocationInfo = new LocationInfo(0, 0);
-        Map<LocationInfo, Integer> resultMap = new HashMap<>();
-        int sum = 0;
-        if (pacLocationInfoList != null) {
-            for (LocationInfo p : pacLocationInfoList) {
-                sum += Math.abs(p.getX() - myLocationInfo.getX());
-                sum += Math.abs(p.getY() - myLocationInfo.getY());
-                resultMap.put(p, sum);
-                sum = 0;
-            }
-            // 将HashMap按照距离排序
-            Map<LocationInfo, Integer> sorted = resultMap
-                    .entrySet()
-                    .stream()
-                    .sorted(comparingByValue())
-                    .collect(toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e2, LinkedHashMap::new));
-            // 取出距离最短的对象位置
-            resultLocationInfo = sorted.keySet().stream().findFirst().orElse(null);
-        }
-        return resultLocationInfo;
     }
 
     /**
@@ -171,5 +112,13 @@ public class CommentUtils {
             return Constants.DOWN;
         }
         return Constants.STAY;
+    }
+
+    /**
+     * 计算两点间的距离：“曼哈顿”法，坐标分别取差值相加
+     */
+    private static int getDistance(LocationInfo endLocation, LocationInfo startLocation) {
+        return Math.abs(endLocation.x - startLocation.x)
+                + Math.abs(endLocation.y - startLocation.y);
     }
 }
